e5a600be0f7ea0a4f4bf82da048fe32b1f2c471f
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-03-26 18:37:01 +0530

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import {useEffect} from "react";
 
-useEffect(() => {
-  
-}, [])
-
-
 function App() {
+  useEffect(() => {
+    const fetchJokke = async () =>( 
+      await fetch(`http://api.icndb.com/jokes/random`). //fetching joke
+      then(res => res.json()) //json object
+      .then(data =>{
+        console.log(data.value.joke); //then whatever data we are having from that data we are returning value.joke 
+        //if you didn't understand copy api url and go to that url and see response
+      })
+    )
+    fetchJokke();
+  }, [])
+  
   return (
     <div className="app">
       <center>
