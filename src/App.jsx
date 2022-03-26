a8d15b5f67a19a99ed3c18dd1d00f5f7263d47a9
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-03-26 18:21:09 +0530

    inital setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="app">
+      <center>
+        <h1>The Joke Generator</h1>
+        <button>Generate Joke</button>
+      </center>
+    </div>
+  )
+}
+
+export default App
