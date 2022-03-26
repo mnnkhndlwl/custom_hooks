51815e7694c10210c0242bb81469275d14a977a7
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-03-26 19:29:32 +0530

    cpmpleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
+import { useState,useRef } from "react";
 import useRandomJoke from "./useRandomJoke";
 
 function App() {
 
-  const joke = useRandomJoke("manan","khandelwal");
-  
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
 
+ 
+  
+  const [firstName, setFirstName] = useState("elon");
+  const [lastName, setLastName] = useState("musk");
+
+  const generate = (e) =>{
+    e.preventDefault();
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
+  const joke = useRandomJoke(firstName,lastName);
   return (
     <div className="app">
       <center>
@@ -13,9 +24,9 @@ function App() {
 
           <h2>{joke}</h2>
           <form >
-            <input placeholder="first name" />
-            <input placeholder="last name" />
-            <button>Generate Joke</button>
+            <input placeholder="first name" ref={firstNameRef}/>
+            <input placeholder="last name" ref={lastNameRef}/>
+            <button onClick={generate}>Generate Joke</button>
           </form>
       </center>
     </div>
